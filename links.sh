c65dd99f7e7e8d8dc2127795a44a48dca5aeae78
Chris Jones <chris.jones@outlook.com>
2013-11-04 03:27:35 +0000

    links

--- links.sh
+++ links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -s /srv/salt-stack/pillar /srv/pillar/
-ln -s /srv/salt-stack/roots /srv/salt
+ln -s /srv/salt-states/pillar /srv/pillar
+ln -s /srv/salt-states/roots /srv/salt
 
-/bin/cp -i /srv/salt-stack/minion /etc/salt/minion
+/bin/cp /srv/salt-states/minion /etc/salt/minion
 
